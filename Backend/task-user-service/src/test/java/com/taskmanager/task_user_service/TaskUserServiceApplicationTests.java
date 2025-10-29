5d9c6095a5b682ea2a246480c969947cfdcc9443
Manojit Malik <manojitmalik15@gmail.com>
2025-10-30 00:45:25 +0530

    First Commit for user service

---
+++ Backend/task-user-service/src/test/java/com/taskmanager/task_user_service/TaskUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskmanager.task_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
